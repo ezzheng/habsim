ddf4cdf4b903d71f9910717dd7e16e2116d68a3b
Emmanuel <ezzheng12345@gmail.com>
2025-10-31 02:43:30 -0700

    Compress elevation grid generation and limit front-end models

--- save_elevation.py
+++ save_elevation.py
@@ -1,12 +1,13 @@
 from PIL import Image
 import numpy as np
 
-Image.MAX_IMAGE_PIXELS = 138240000
+Image.MAX_IMAGE_PIXELS = 1_000_000_000  # raise the limit above the TIFF size
 
 def tif_to_npy(tif_path, npy_path):
     with Image.open(tif_path) as img:
-        img_array = np.array(img)
-    np.save(npy_path, img_array)
+        img_half = img.resize((img.width // 2, img.height // 2), resample=Image.BILINEAR)
+        img_array = np.array(img_half, dtype=np.int16)
+    np.save(npy_path, img_array, allow_pickle=False)
 
 # Mean - 30 arc seconds
-tif_to_npy('./30N150W_20101117_gmted_mea300.tif', './gefs/worldelev.npy')
+tif_to_npy('data/gmted_tif/mn30_grd.tif', 'data/worldelev.npy')
